2ac55001174cad4d60f55c7fe2c71b2327db4491
Harvey Chua <harvs89.2@gmail.com>
2018-12-21 13:25:35 +0800

    Add script to pull vendor blobs

---
+++ pull.py
@@ -0,0 +1,28 @@
+#!/bin/python
+#pull from vendor repo
+#assumes that script is located in los15/hardware/qcom/camera-op3/util/pull
+
+from shutil import copy2
+import os
+VENDOR_DIR = "../../../vendor/oneplus-cm-14.1"
+
+systemdir = os.getcwd() + "/system/"
+def pull(line):
+    copy2(VENDOR_DIR + "/oneplus3/proprietary/" + line,  systemdir + line)
+
+def main():
+    print "Opening proprietary-files.txt"
+    f = open("proprietary-files.txt", "r")
+    for line in f:
+        line = line[0:len(line)-1]
+        
+        #remove hash
+        if line.find("|") != -1:
+            line = line[0:len(line)-41]
+        if line.find("#") == -1 and line != "":
+            print line
+            if not os.path.isdir(systemdir + os.path.dirname(line)):
+                os.makedirs(systemdir + os.path.dirname(line))
+            pull(line)
+            
+main()
